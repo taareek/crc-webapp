f55c9836c2fc1f80e10bf6cbf8a2f5405578f0ea
Tarek Aziz <tarekiub17@gmail.com>
2023-12-02 22:54:45 +0600

    main and utils updates

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 from typing import Optional
 from fastapi import FastAPI, File, UploadFile, Request, Form
-# import utils
-# import utils_main
+import utils
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
@@ -12,6 +11,22 @@ app = FastAPI()
 # mounting images and templates 
 app.mount("/static", StaticFiles(directory= "static"), name="static")  
 template = Jinja2Templates(directory= "templates")   
+
+# @app.get("/")
+# async def home(request: Request):
+#     return template.TemplateResponse("index.html", {"request":request})
+
+# @app.post("/")
+# async def demo_home(request: Request, file:UploadFile= File(...)):
+    
+#     result = None
+#     error = None
+
+#     try:
+#         result = utils.get_results(input_img=file)
+#     except Exception as e:
+#         error = e
+#     return template.TemplateResponse("prediction.html", {"request":request, "result":result, "error":error})
 
 
 @app.get("/")
--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
 import feat_extractor
 import pandas as pd
 import pickle
-from skimage import io
+from skimage import io as imp
 import numpy as np
+import io
+import base64
+from PIL import Image
 
 # load feature RF-RFE feature selector 
 fs_path = "./models/rfe.pkl"
@@ -49,21 +52,38 @@ label_dict = {
     7: "EMPTY"
 }
 
+# image transformation 
+def img_transform(input_img):
+    img_bytes = input_img.file.read()
+    img = Image.open(io.BytesIO(img_bytes))  # it needs then convert into numpy to work with opencv
+    cv_img = np.array(img)
 
-img = io.imread('./static/image1.png',0)
+    # encoded image 
+    encoded_string = base64.b64encode(img_bytes)
+    bs64 = encoded_string.decode('utf-8')
+    encoded_img = f'data:image/jpeg;base64,{bs64}'
+    return cv_img, encoded_img
+
+
+# img = imp.imread('./static/image1.png',0)
 
 # getting features from the input image 
-features = feat_extractor.get_features(img).flatten()
+# features = feat_extractor.get_features(img).flatten()
 
 # making a dataframe 
-f_data = []
-header = [f"f_{i}" for i in range(1, 1025)]
-f_data.append(features)
-f_df = pd.DataFrame(f_data, columns=header)
-print("original features: ",features.shape)
+# f_data = []
+# header = [f"f_{i}" for i in range(1, 1025)]
+# f_data.append(features)
+# f_df = pd.DataFrame(f_data, columns=header)
+# print("original features: ",features.shape)
 
 # feature selection 
-def get_selected_ft(df):
+def get_selected_ft(all_features):
+    f_data = []
+    header = [f"f_{i}" for i in range(1, 1025)]
+    f_data.append(all_features)
+    df = pd.DataFrame(f_data, columns=header)
+
     s_f = fs_rfe.support_
     selected_features = []
 
@@ -74,12 +94,18 @@ def get_selected_ft(df):
     return df
 
 # getting selected features 
-sf = get_selected_ft(f_df)
-print("selected feature shape: ", sf.shape)
+# sf = get_selected_ft(features)
+# print("selected feature shape: ", sf.shape)
 
 
 # get prediction from the stacking-ensemble model 
-def get_prediction(selected_ft):
+def get_prediction(img):
+    
+    # getting features from the input image 
+    features = feat_extractor.get_features(img).flatten()
+    # getting selected features 
+    selected_ft = get_selected_ft(features)
+
     probs_model1_test = knn_model.predict_proba(selected_ft)
     probs_model2_test = svm_model.predict_proba(selected_ft)
     probs_model3_test = rfc_model.predict_proba(selected_ft)
@@ -91,8 +117,29 @@ def get_prediction(selected_ft):
 
     pred_p = stack_model.predict_proba(meta_features_test)
     pred_p = max(pred_p.flatten())
-    print(pred_p*100)
-    print("The predicted clas is", label_dict[int(pred[0])])
+    # print(pred_p*100)
+    pred_proba = round(pred_p * 100, 2)
+    # print("The predicted clas is", label_dict[int(pred[0])])
+    pred_class = label_dict[int(pred[0])]
 
-print("Final prediction: \n")
-get_prediction(sf)
+    return pred_class, pred_proba
+
+# print("Final prediction: \n")
+# get_prediction(sf)
+
+## get prediction results by input image
+def get_results(input_img, is_api=False):
+    # read image 
+    org_img, org_encoded_img = img_transform(input_img)
+    pred_class, pred_proba = get_prediction(org_img)
+
+    pred_results = {
+            "class_name": pred_class,
+            "class_probability": pred_proba
+        }
+    
+    # conditionally add image data to the result dictionary
+    if not is_api:
+        pred_results["org_encoded_img"] = org_encoded_img
+
+    return pred_results
